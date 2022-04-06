aba0096c4210670d4a22f8541af127da15a906f8
MasaGt <MasaGt@users.noreply.github.com>
2022-04-07 09:45:37 +1200

    added 404 page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ import {
 import Home from "./pages/Home";
 import SEPractice from "./pages/SE-Practice";
 import SubmitArticle from "./pages/Submit-Article"; 
-// import NotFoundPage from "./pages/404";
+import NotFoundPage from "./pages/404";
 
 const App = () =>  {
   return (
@@ -27,6 +27,7 @@ const App = () =>  {
             <Route exact path="/" element={<Home />}/>
             <Route  path="/SEPractice" element={<SEPractice />}/>
             <Route  path="/SubmitArticle" element={<SubmitArticle />}/>
+            <Route  path="*" element={<NotFoundPage />}/>
           </Routes>
         </div>
       </div>
